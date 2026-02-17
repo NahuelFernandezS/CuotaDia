83e6d5ce91d0cdea4bec616a0b56f587cf95e0a5
Nahuel Fernandez S <nahuel_fernandez20@hotmail.com>
2026-02-16 23:06:05 -0300

    Fix GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LoanSimulator from './components/LoanSimulator';
 import ApplicationWizard from './components/ApplicationWizard';
 
